fd81aba1e2c78e038ec38c9c78a32f4c7787f83f
Yuwen He <yuwen.he@vg.no>
2016-03-08 11:11:48 +0100

    Test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var stockRepository = require('./stockRepository')();
 var app = require('./app')(stockRepository);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('listening on port 3000!');
 });
